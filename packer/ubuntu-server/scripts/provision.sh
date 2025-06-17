6d309c6d0ea43074ab793f6151dae0e07f5dbfb8
Alis Akers <alismxwll@gmail.com>
2025-06-17 08:36:25 -0700

    fix: remove branch specification from dibbs-vm clone command
    The change removes the hardcoded branch (-b alis/gcp) flag to clone the default branch instead.

--- packer/ubuntu-server/scripts/provision.sh
+++ packer/ubuntu-server/scripts/provision.sh
@@ -30,7 +30,7 @@ else
 fi
 
 # Clone the dibbs-vm repository
-git clone -b alis/gcp https://github.com/CDCgov/dibbs-vm.git
+git clone https://github.com/CDCgov/dibbs-vm.git
 cd "$HOME/dibbs-vm/$DIBBS_SERVICE" || exit
 
 # ensures the DIBBS variables are set and accessible to the wizard
