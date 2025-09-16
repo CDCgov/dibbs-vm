b32f8a7ed7a117c1c727a198a16c5f513ae73253
Alis Akers <alismxwll@gmail.com>
2025-09-16 12:10:28 -0700

    feat: set default bucket and prevent overwriting existing files in GCP uploads

--- gcp_upload.sh
+++ gcp_upload.sh
@@ -5,7 +5,7 @@ set -x
 
 service=$1
 version=$2
-bucket=$3
+bucket=${3:-"skylight-dibbs-vm-images"}
 # accept an argument or use the default to "gcp raw"
 build_types=(gcp raw)
 gitsha=$(git rev-parse --short HEAD)
@@ -13,8 +13,8 @@ gitsha=$(git rev-parse --short HEAD)
 if [ -z "$service" ] || [ -z "$version" ]; then
   echo "Remember to log into gcloud before running this script."
   echo "Usage: ./gcp_upload.sh [DIBBS_SERVICE] [DIBBS_VERSION] [BUCKET_NAME]"
-  echo "Example: ./gcp_upload.sh dibbs-ecr-viewer 1.0.0 dibbs-vm-bucket"
-  echo "Example: ./gcp_upload.sh dibbs-query-connector 1.0.0 dibbs-vm-images"
+  echo "Example: ./gcp_upload.sh dibbs-ecr-viewer 1.0.0 skylight-dibbs-vm-images"
+  echo "Example: ./gcp_upload.sh dibbs-query-connector 1.0.0 skylight-dibbs-vm-images"
 exit 1
 fi
 
@@ -27,6 +27,12 @@ for build_type in "${build_types[@]}"; do
     echo "Build directory for that version does not exist."
     exit 1
   fi
+  gsutil ls gs://$bucket/ubuntu-2404-$service-$build_type-$version-$gitsha.tar.gz && {
+    echo "File already exists in the bucket. Exiting to avoid overwrite."
+    exit 1
+  } || {
+    echo "File does not exist in the bucket. Continuing with upload."
+  }
 
   rm disk.raw || true
   rm ubuntu-2404-$service-$build_type-$version-$gitsha.tar.gz || true
